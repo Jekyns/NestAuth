ca6d387562309b5e20311137b826b93e79b50104
Alexandr <axkuz97@yandex.ru>
2021-06-08 03:45:05 +0300

    fix auth

--- src/modules/users/user.entity.ts
+++ src/modules/users/user.entity.ts
@@ -11,7 +11,7 @@ export class User extends Model<User> {
     @Column({
         type: DataType.STRING,
         unique: true,
-        allowNull: false,
+        allowNull: true,
     })
     email: string;
 
