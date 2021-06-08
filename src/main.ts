18f624c45f6826bbeb9c47308f65959f4566e335
Alexandr <axkuz97@yandex.ru>
2021-06-08 04:36:02 +0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
-  app.useGlobalPipes(new ValidationPipe()); 
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('My First NestJS project')
+    .setDescription('The My first NestJs API description')
+    .setVersion('1.0')
+    .addTag('nestjs')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
